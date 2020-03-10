a98182c4fb38147ba21beb2313f8ba2b332a497f
Ciprian Danis <cdanis39@gmail.com>
2020-03-10 19:28:08 +0200

    Made a crude contact page with corresponding CSS file

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
+"use strict";
+
 function openNavBar() {
     document.getElementById("sidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
-  
+
 function closeNavBar() {
     document.getElementById("sidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("main").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
 }
