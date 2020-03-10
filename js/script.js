1c9cddfcb09203614103b6f97f94d4664ecb6623
Ioan <ioan.sava28@gmail.com>
2020-03-10 21:58:20 +0200

    Home page v1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function openNavBar() {
-    document.getElementById("sidenav").style.width = "20%";
-    document.getElementById("main").style.marginLeft = "20%";
+    document.getElementById("sidenav").style.width = "15%";
+    document.getElementById("main").style.marginLeft = "15%";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
   
