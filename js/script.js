495f7bc6956db9a95afc8cec236d605388d45527
Ioan <ioan.sava28@gmail.com>
2020-03-11 20:58:03 +0200

    Home page (responsive)

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 function openNavBar() {
     document.getElementById("sidenav").style.width = "15%";
-    document.getElementById("main").style.marginLeft = "15%";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("mainid").style.marginLeft = "15%";
+    document.body.style.backgroundColor = "#a7e9af";
 }
   
 function closeNavBar() {
     document.getElementById("sidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-    document.body.style.backgroundColor = "white";
+    document.getElementById("mainid").style.marginLeft= "0";
+    document.body.style.backgroundColor = "#eef9bf";
 }
