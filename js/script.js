d2cb468f3e92ec8ebe473476437376e22a422200
Ioan <ioan.sava28@gmail.com>
2020-03-11 21:27:55 +0200

    Responsive menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 function openNavBar() {
-    document.getElementById("sidenav").style.width = "15%";
-    document.getElementById("mainid").style.marginLeft = "15%";
+    var screenWidth = window.innerWidth;
+
+    if (screenWidth < 620) {
+        document.getElementById("sidenav").style.width = "100%";
+    }
+    else if (screenWidth < 960) {
+        document.getElementById("sidenav").style.width = "35%";
+        document.getElementById("mainid").style.marginLeft = "35%";
+    }
+    else {
+        document.getElementById("sidenav").style.width = "15%";
+        document.getElementById("mainid").style.marginLeft = "15%";
+    }
     document.body.style.backgroundColor = "#a7e9af";
 }
   
